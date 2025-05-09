3c1f51b431112667d21cb08e5bdd2391bd83ec27
youssfmadani <youssfelmadani@gmail.com>
2025-05-09 15:17:53 +0100

    Ajout interface ProduitRepository

---
+++ src/main/java/com/example/pharmac/repository/ProduitRepository.java
@@ -0,0 +1,11 @@
+package com.example.pharmac.repository;
+
+import com.example.pharmac.model.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+
+}
