b05e9818607aa7bb4e03af074d219acc6ffb1b8a
youssfmadani <youssfelmadani@gmail.com>
2025-05-09 15:08:58 +0100

    Initialisation de projet Spring boot avec class main

---
+++ src/main/java/com/example/pharmac/PharmacApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pharmac/PharmacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
